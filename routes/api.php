a56285f9ec2e20e7d1cb75f426e773fe67e07712
nileshsutariya <nilesh.netdeveloper@gmail.com>
2025-03-28 17:18:15 +0530

    user customer api

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\admin\loginController;
 use App\Http\Controllers\API\customer\registerController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
+
+// Route::post('admin/login', [UserController::class, 'login']);
+
+Route::post('admin/update', [UserController::class, 'update']);
+Route::post('admin/list', [UserController::class, 'list']);
+Route::post('admin/customer/update', [CustomerController::class, 'customerupdate']);
+Route::post('admin/customer/list', [CustomerController::class, 'customerlist']);
+
+Route::group([
+    'middleware' => 'login',
+], function ($router) {
 
     Route::post('customer/register', [registerController::class, 'register']);
     Route::post('customer/register', [registerController::class, 'register']);
