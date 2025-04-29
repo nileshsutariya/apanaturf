fa504ce0915162605cd855c1de832b9a1dde0690
nilesh sutariya <nilesh.netdeveloper@gmail.com>
2025-04-29 16:51:04 +0530

    structurechnages

--- routes/api.php
+++ routes/api.php
@@ -24,60 +24,85 @@ use App\Http\Controllers\API\User\ConfigurationController;
 //         'middleware' => 'login',
 //     ], function ($router) {
 
-Route::post('admin/login', [loginController::class, 'login']);
-Route::post('admin/logout', [loginController::class, 'logout']);
-// });
-Route::post('admin/update', [UserController::class, 'update']);
-Route::post('admin/list', [UserController::class, 'list']);
+Route::prefix('admin')->group(function () {
 
-Route::post('admin/customer/update', [CustomerController::class, 'customerupdate']);
-Route::post('admin/customer/list', [CustomerController::class, 'customerlist']);
 
-Route::post('admin/sports/add', [ConfigurationController::class, 'addsports']);
-Route::post('admin/sports/list', [ConfigurationController::class, 'sportlist']);
-Route::post('admin/sports/delete', [ConfigurationController::class, 'sportdelete']);
+    Route::post('/login', [loginController::class, 'login']);
+    Route::post('/logout', [loginController::class, 'logout']);
 
-Route::post('admin/amenities/add', [ConfigurationController::class, 'addamenities']);
-Route::post('admin/amenities/list', [ConfigurationController::class, 'amenitieslist']);
-Route::post('admin/amenities/delete', [ConfigurationController::class, 'amenitiesdelete']);
-
-Route::post('admin/coupons/add', [CouponsController::class, 'addcoupons']);
-Route::post('admin/coupons/delete', [CouponsController::class, 'deletecoupons']);
-Route::post('admin/coupons/list', [CouponsController::class, 'listcoupons']);
-
-Route::post('admin/banner/add', [BannersController::class, 'addbanner']);
-Route::post('admin/banner/delete', [BannersController::class, 'bannerdelete']);
-Route::post('admin/banner/list', [BannersController::class, 'bannerlist']);
-
-// Route::group([
-    //     'middleware' => 'customer.login',
-    // ], function () {
-        Route::prefix('customer')->group(function () {
-        
-        
-        Route::post('/register', [RegisterController::class, 'register']);
-        Route::post('/profile', [RegisterController::class, 'profile']);
-        
-        Route::post('/banner', [BannerController::class, 'banners']);        
-        Route::post('/coupons', [CouponController::class, 'coupons']);
-        Route::post('/venues', [VenuesController::class, 'venues']);
-        
-        
-        Route::prefix('/turf')->controller(TurfController::class)->group(function () {
-            Route::post('/', 'index');
-            Route::post('/details', 'details');
-        });
-
-        Route::prefix('/login')->controller(CustomerLoginContoller::class)->group(function () {
-            Route::post('/', 'login');
-            Route::post('/otpverify', 'otpverify');
-            Route::post('/otpresend', 'otpresend');
-        });
-
-        Route::middleware('customer.login')->group(function () {
-            Route::post('/logout', [CustomerLoginContoller::class, 'logout']);
-        });
+    Route::post('/update', [UserController::class, 'update']);
+    Route::post('/list', [UserController::class, 'list']);
+    
+    Route::prefix('/customer')->controller(CustomerController::class)->group(function () {
+        Route::post('/store', 'store');
+        Route::post('/list', 'list');
     });
+    Route::prefix('/sports')->controller(ConfigurationController::class)->group(function () {
+        Route::post('/store', 'SportStore');
+        Route::post('/list', 'SportList');
+        Route::post('/delete', 'SportsDelete');
+    });
+    Route::prefix('/amenities')->controller(ConfigurationController::class)->group(function () {
+        Route::post('/store', 'AmenitiesStore');
+        Route::post('/list', 'AmenitiesList');
+        Route::post('/delete', 'AmenitiesDelete');
+    });
+    Route::prefix('/coupons')->controller(CouponsController::class)->group(function () {
+        Route::post('/store', 'store');
+        Route::post('/list', 'list');
+        Route::post('/delete', 'delete');
+    });
+    Route::prefix('/banners')->controller(BannersController::class)->group(function () {
+        Route::post('/store', 'store');
+        Route::post('/list', 'list');
+        Route::post('/delete', 'delete');
+    });
+
+    // Route::post('/sports/add', [ConfigurationController::class, 'addsports']);
+    // Route::post('/sports/list', [ConfigurationController::class, 'sportlist']);
+    // Route::post('/sports/delete', [ConfigurationController::class, 'sportdelete']);
+
+    // Route::post('/amenities/add', [ConfigurationController::class, 'addamenities']);
+    // Route::post('/amenities/list', [ConfigurationController::class, 'amenitieslist']);
+    // Route::post('/amenities/delete', [ConfigurationController::class, 'amenitiesdelete']);
+
+    // Route::post('/coupons/add', [CouponsController::class, 'addcoupons']);
+    // Route::post('/coupons/delete', [CouponsController::class, 'deletecoupons']);
+    // Route::post('/coupons/list', [CouponsController::class, 'listcoupons']);
+
+    // Route::post('/banner/add', [BannersController::class, 'addbanner']);
+    // Route::post('/banner/delete', [BannersController::class, 'bannerdelete']);
+    // Route::post('/banner/list', [BannersController::class, 'bannerlist']);
+});
+// Route::group([
+//     'middleware' => 'customer.login',
+// ], function () {
+Route::prefix('customer')->group(function () {
+
+
+    Route::post('/register', [RegisterController::class, 'register']);
+    Route::post('/profile', [RegisterController::class, 'profile']);
+
+    Route::post('/banner', [BannerController::class, 'banners']);
+    Route::post('/coupons', [CouponController::class, 'coupons']);
+    Route::post('/venues', [VenuesController::class, 'venues']);
+
+
+    Route::prefix('/turf')->controller(TurfController::class)->group(function () {
+        Route::post('/', 'index');
+        Route::post('/details', 'details');
+    });
+
+    Route::prefix('/login')->controller(CustomerLoginContoller::class)->group(function () {
+        Route::post('/', 'login');
+        Route::post('/otpverify', 'otpverify');
+        Route::post('/otpresend', 'otpresend');
+    });
+
+    Route::middleware('customer.login')->group(function () {
+        Route::post('/logout', [CustomerLoginContoller::class, 'logout']);
+    });
+});
 
 
 
